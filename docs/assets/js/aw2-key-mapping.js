29fdfdf84f37fa3583badfa87d63cb1934826c67
Lukas Theis <muwum@muwum.net>
2022-04-21 23:27:55 +0200

    add support for different keyboard layouts

--- docs/assets/js/aw2-key-mapping.js
+++ docs/assets/js/aw2-key-mapping.js
@@ -1,193 +1,27 @@
 (async function () {
-  const keyCodes = {
-    0: 'That key has no keycode',
-    3: 'break',
-    8: 'backspace / delete',
-    9: 'tab',
-    12: 'clear',
-    13: 'enter',
-    16: 'shift',
-    17: 'ctrl',
-    18: 'alt',
-    19: 'pause/break',
-    20: 'caps lock',
-    21: 'hangul',
-    25: 'hanja',
-    27: 'escape',
-    28: 'conversion',
-    29: 'non-conversion',
-    32: 'spacebar',
-    33: 'page up',
-    34: 'page down',
-    35: 'end',
-    36: 'home',
-    37: 'left arrow',
-    38: 'up arrow',
-    39: 'right arrow',
-    40: 'down arrow',
-    41: 'select',
-    42: 'print',
-    43: 'execute',
-    44: 'Print Screen / F13 (firefox)',
-    45: 'insert',
-    46: 'delete',
-    47: 'help',
-    48: '0',
-    49: '1',
-    50: '2',
-    51: '3',
-    52: '4',
-    53: '5',
-    54: '6',
-    55: '7',
-    56: '8',
-    57: '9',
-    58: ':',
-    59: 'semicolon (firefox), equals',
-    60: '<',
-    61: 'equals (firefox)',
-    63: 'ß',
-    64: '@ (firefox)',
-    65: 'a',
-    66: 'b',
-    67: 'c',
-    68: 'd',
-    69: 'e',
-    70: 'f',
-    71: 'g',
-    72: 'h',
-    73: 'i',
-    74: 'j',
-    75: 'k',
-    76: 'l',
-    77: 'm',
-    78: 'n',
-    79: 'o',
-    80: 'p',
-    81: 'q',
-    82: 'r',
-    83: 's',
-    84: 't',
-    85: 'u',
-    86: 'v',
-    87: 'w',
-    88: 'x',
-    89: 'y',
-    90: 'z',
-    91: 'Windows Key / Left ⌘ / Chromebook Search key',
-    92: 'right window key',
-    93: 'Windows Menu / Right ⌘',
-    95: 'sleep',
-    96: 'numpad 0',
-    97: 'numpad 1',
-    98: 'numpad 2',
-    99: 'numpad 3',
-    100: 'numpad 4',
-    101: 'numpad 5',
-    102: 'numpad 6',
-    103: 'numpad 7',
-    104: 'numpad 8',
-    105: 'numpad 9',
-    106: 'multiply',
-    107: 'add',
-    108: 'numpad period (firefox)',
-    109: 'subtract',
-    110: 'decimal point',
-    111: 'divide',
-    112: 'f1',
-    113: 'f2',
-    114: 'f3',
-    115: 'f4',
-    116: 'f5',
-    117: 'f6',
-    118: 'f7',
-    119: 'f8',
-    120: 'f9',
-    121: 'f10',
-    122: 'f11',
-    123: 'f12',
-    124: 'f13',
-    125: 'f14',
-    126: 'f15',
-    127: 'f16',
-    128: 'f17',
-    129: 'f18',
-    130: 'f19',
-    131: 'f20',
-    132: 'f21',
-    133: 'f22',
-    134: 'f23',
-    135: 'f24',
-    136: 'f25',
-    137: 'f26',
-    138: 'f27',
-    139: 'f28',
-    140: 'f29',
-    141: 'f30',
-    142: 'f31',
-    143: 'f32',
-    144: 'num lock',
-    145: 'scroll lock',
-    151: 'airplane mode',
-    160: '^',
-    161: '!',
-    162: '؛ (arabic semicolon)',
-    163: '#',
-    164: '$',
-    165: 'ù',
-    166: 'page backward',
-    167: 'page forward',
-    168: 'refresh',
-    169: 'closing paren (AZERTY)',
-    170: '*',
-    171: '~ + * key',
-    172: 'home key',
-    173: 'minus (firefox), mute/unmute',
-    174: 'decrease volume level',
-    175: 'increase volume level',
-    176: 'next',
-    177: 'previous',
-    178: 'stop',
-    179: 'play/pause',
-    180: 'e-mail',
-    181: 'mute/unmute (firefox)',
-    182: 'decrease volume level (firefox)',
-    183: 'increase volume level (firefox)',
-    186: 'semi-colon / ñ',
-    187: 'equal sign',
-    188: 'comma',
-    189: 'dash',
-    190: 'period',
-    191: 'forward slash / ç',
-    192: 'grave accent / ñ / æ / ö',
-    193: '?, / or °',
-    194: 'numpad period (chrome)',
-    219: 'open bracket',
-    220: 'back slash',
-    221: 'close bracket / å',
-    222: 'single quote / ø / ä',
-    223: '`',
-    224: 'left or right ⌘ key (firefox)',
-    225: 'altgr',
-    226: '< /git >, left back slash',
-    230: 'GNOME Compose Key',
-    231: 'ç',
-    233: 'XF86Forward',
-    234: 'XF86Back',
-    235: 'non-conversion',
-    240: 'alphanumeric',
-    242: 'hiragana/katakana',
-    243: 'half-width/full-width',
-    244: 'kanji',
-    251: 'unlock trackpad (Chrome/Edge)',
-    255: 'toggle touchpad',
-  }
 
+  let selectedLayout = "us";
   let keyMap = {};
   try {
     keyMap = (JSON.parse(localStorage.getItem('aw2-settings-key-mapping'))) || {};
   } catch (error) {
     console.error(error);
+  }
+  try {
+    selectedLayout = (localStorage.getItem('aw2-settings-key-layout')) || "us";
+  } catch (error) {
+    console.error(error);
+  }
+
+  function keyFromDefault(def) {
+    const res = Object.entries(window.aw2KeyboardLayouts.us).find(function (a) { return a[1] === def });
+    if (res) {
+      return res[0];
+    }
+    return null
+  }
+  function localKey(key) {
+    return window.aw2KeyboardLayouts[selectedLayout][key];
   }
 
   function checkSettingsFromLocalStorage() {
@@ -201,6 +35,15 @@
     } catch (error) {
       console.error(error);
     }
+    try {
+      const newSelectedLayout = (localStorage.getItem('aw2-settings-key-layout')) || "us";
+      if (newSelectedLayout !== selectedLayout) {
+        selectedLayout = newSelectedLayout;
+        updateKeymaps();
+      }
+    } catch (error) {
+      console.error(error);
+    }
     setTimeout(checkSettingsFromLocalStorage, 1000);
   }
 
@@ -208,6 +51,9 @@
     localStorage.setItem('aw2-settings-key-mapping', JSON.stringify(keyMap));
   }
 
+  function saveSelectedLayout() {
+    localStorage.setItem('aw2-settings-key-layout', selectedLayout);
+  }
   function loadSkills() {
     const aw2Skills = Array.from(document.querySelectorAll('[data-aw2-skill]'));
     aw2Skills.forEach(function (key) {
@@ -222,9 +68,9 @@
         key.appendChild(armoryElement);
         const showKey = document.createElement("span");
         showKey.classList.add("aw2-show-key");
-        showKey.textContent = key.getAttribute("data-aw2-key-mapped") || key.getAttribute("data-aw2-key");
+        showKey.textContent = key.getAttribute("data-aw2-key-mapped") || localKey(keyFromDefault(key.getAttribute("data-aw2-key"))) ;
         var observer = new MutationObserver(function (mutations) {
-          showKey.textContent = key.getAttribute("data-aw2-key-mapped") || key.getAttribute("data-aw2-key");
+          showKey.textContent = key.getAttribute("data-aw2-key-mapped") || localKey(keyFromDefault(key.getAttribute("data-aw2-key")));
         });
         observer.observe(key, {
           attributes: true,
@@ -253,12 +99,16 @@
   function updateKeymaps() {
     const aw2Skills = Array.from(document.querySelectorAll('[data-aw2-key]'));
     aw2Skills.forEach(function (key) {
-      const k = key.getAttribute('data-aw2-key');
+      const k = keyFromDefault(key.getAttribute('data-aw2-key'));
       if (keyMap[k]) {
         hasCustomKeyMapping = true;
         key.setAttribute('data-aw2-key-mapped', keyMap[k])
       } else {
         key.removeAttribute('data-aw2-key-mapped');
+      }
+      const showKey = key.querySelector(".aw2-show-key");
+      if(showKey){
+        showKey.textContent = key.getAttribute("data-aw2-key-mapped") || localKey(k);
       }
     });
   }
@@ -279,6 +129,23 @@
       aw2NoJsNotice.style.display = "none";
       const keyBindingList = settingsHook.querySelector(".aw2-key-binding-list");
       keyBindingList.style.display = "block";
+
+      const aw2KeyboardLayout = keyBindingList.querySelector("#aw2-keyboard-layout");
+      aw2KeyboardLayout.value = selectedLayout;
+      aw2KeyboardLayout.addEventListener("change",function(event){
+        if(aw2KeyboardLayout.value !== selectedLayout){
+          selectedLayout = aw2KeyboardLayout.value;
+          saveSelectedLayout();
+          aw2SkillKeySettings.forEach(function (aw2SkillKeySettingInput) {
+            const key = keyFromDefault(aw2SkillKeySettingInput.getAttribute("data-aw2-settings-key"));
+            aw2SkillKeySettingInput.setAttribute("placeholder", localKey(key));
+            if (keyMap[key]) {
+              aw2SkillKeySettingInput.value = keyMap[key];
+            }
+          });
+        }
+      });
+
       const aw2SkillKeySettings = Array.from(document.querySelectorAll('[data-aw2-settings-key]'));
       function skillSettingKeyDownHandler(event) {
         if (event instanceof MouseEvent) {
@@ -286,7 +153,7 @@
             event.preventDefault();
             console.log(event, event.buttons);
             if (buttons[event.buttons]) {
-              const defaultKey = event.target.getAttribute("data-aw2-settings-key");
+              const defaultKey = keyFromDefault(event.target.getAttribute("data-aw2-settings-key"));
               keyMap[defaultKey] = buttons[event.buttons];
               saveKeyMap();
               event.target.value = keyMap[defaultKey];
@@ -294,10 +161,13 @@
           }
         } else if (event instanceof KeyboardEvent) {
           event.preventDefault();
+          const defaultKey = keyFromDefault(event.target.getAttribute("data-aw2-settings-key"));
           if (["Shift", "Alt", "Control"].includes(event.key)) {
+            keyMap[defaultKey] = localKey(event.code);
+            saveKeyMap();
+            event.target.value = keyMap[defaultKey];
             return;
           }
-          const defaultKey = event.target.getAttribute("data-aw2-settings-key");
           if (event.key === "Escape") {
             delete keyMap[defaultKey];
             saveKeyMap();
@@ -326,7 +196,7 @@
           }else{
             modifiers.push(event.key);
           }*/
-          const keyLabel = keyCodes[event.keyCode];
+          const keyLabel = localKey(event.code);
           if (keyLabel.includes(" ")) {
             modifiers.push('(' + keyLabel + ')');
           } else {
@@ -342,8 +212,8 @@
       aw2SkillKeySettings.forEach(function (aw2SkillKeySettingInput) {
         aw2SkillKeySettingInput.addEventListener("keydown", skillSettingKeyDownHandler);
         aw2SkillKeySettingInput.addEventListener("mousedown", skillSettingKeyDownHandler);
-        const key = aw2SkillKeySettingInput.getAttribute("data-aw2-settings-key");
-        aw2SkillKeySettingInput.setAttribute("placeholder", key);
+        const key = keyFromDefault(aw2SkillKeySettingInput.getAttribute("data-aw2-settings-key"));
+        aw2SkillKeySettingInput.setAttribute("placeholder", localKey(key));
         if (keyMap[key]) {
           aw2SkillKeySettingInput.value = keyMap[key];
         }

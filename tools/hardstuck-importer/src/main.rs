6e78abc6fefa99085e2bf0b6d5accbf32f55827d
Andy Berdan <andy@berdan.ca>
2022-05-26 09:04:12 -0400

    Minor change to allow importer to help me add chat codes to the challenge builds

--- tools/hardstuck-importer/src/main.rs
+++ tools/hardstuck-importer/src/main.rs
@@ -16,13 +16,30 @@ struct Build {
 	example1: String,
 	example2: String,
 	example3: String,
+	chatcode: String,
+	gearcode: String,
+}
+
+fn dump(line: u64, build: Build) {
+	println!("{}: {profession} {category}: {build_url}",
+		line,
+		profession = build.class,
+		category = build.winners,
+		build_url = build.build,
+		// chatcode = build.chatcode,
+	)
 }
 
 fn process() -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
+	let mut line = 0;
     for result in rdr.deserialize() {
+		line += 1;
         let build: Build = result?;
-        println!("{:?}", build);
+        // println!("{:?}", build);
+		if build.chatcode.is_empty() {
+		  dump(line, build);
+		}
     }
     Ok(())
 }

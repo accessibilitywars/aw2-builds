bb5f21fd6e6e3e9df38b684d9fa3bf8882d04993
Andy Berdan <andy@berdan.ca>
2022-06-03 17:45:19 -0400

    Added html templatized output

---
+++ tools/hardstuck-importer/README.md
@@ -0,0 +1,10 @@
+# Accessibility Challenge Importer
+
+- A quick tool to import the hardstuck accessibility challenges
+
+## TODO:
+
+- [x] Convert CSV gw2skills links to chatcodes
+- [x] Parse CSV file
+- [x] Templatize output
+- [ ] Split output, and write unique files for each entry
--- tools/hardstuck-importer/src/main.rs
+++ tools/hardstuck-importer/src/main.rs
@@ -1,8 +1,12 @@
-use std::error::Error;
+use std::error;
 use std::io;
 use std::process;
+use std::process::Command;
 
+use indoc::indoc;
 use serde::Deserialize;
+
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 #[allow(dead_code)]
 #[derive(Debug, Deserialize)]
@@ -21,23 +25,81 @@ struct Build {
 	gearcode: String,
 }
 
+/*
 fn dump(line: u64, build: Build) {
 	println!("{}: {profession} {category}: {data}",
 		line,
 		profession = build.class,
 		category = build.winners,
-		// data = build.build,
 		data = build.chatcode,
 	)
 }
+*/
 
-fn process() -> Result<(), Box<dyn Error>> {
+fn chat2markup(chatcode: String) -> Result<String> {
+	let output = Command::new("chatr")
+		.arg(chatcode)
+		.output()
+		.expect("failed to execute chatr");
+
+	Ok(String::from_utf8_lossy(&output.stdout).into_owned())
+}
+
+fn convert(line: u64, build: Build) {
+	println!("Converting {}: {profession} {category}: {data} -> stdout",
+		line,
+		profession = build.class,
+		category = build.winners,
+		// data = build.build,
+		data = build.chatcode,
+	);
+
+	let markupcode = match chat2markup(build.chatcode.clone()) {
+		Ok(chatcode) => chatcode,
+		Err(_) => "".to_string()
+	};
+
+	println!(indoc! {"
+		---
+		author: {accountname}
+		editor: berdandy
+		title: Accessibility Contest - {profession} {role} {category}
+		tags: {profession}
+		---
+
+		{desc}
+		
+		## Traits and Skills
+		
+		Template Code:
+		
+		`{chatcode}`
+
+		{chatr_output}
+
+		## References
+
+		- {build}
+	"},
+		accountname = build.accountname,
+		profession = build.class,
+		role = build.role,
+		category = build.winners,
+		desc = build.desc,
+		chatcode = build.chatcode,
+		chatr_output = markupcode,
+		build = build.build,
+	);
+}
+
+fn process() -> Result<()> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
 	let mut line = 0;
     for result in rdr.deserialize() {
 		line += 1;
         let build: Build = result?;
-		dump(line, build);
+		convert(line, build);
+		// dump(line, build);
     }
     Ok(())
 }
